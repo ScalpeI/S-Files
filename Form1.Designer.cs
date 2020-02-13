c8b1b6ddda92b25b172ee41e181cdd3def9d7d5f
ScalpeI <60978659+ScalpeI@users.noreply.github.com>
2020-02-13 10:18:41 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace S_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Prz = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reason7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(27, 16);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Prz,
+            this.Reason1,
+            this.Reason2,
+            this.Reason3,
+            this.Reason4,
+            this.Reason5,
+            this.Reason6,
+            this.Reason7,
+            this.Sum});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(782, 429);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellDoubleClick);
+            // 
+            // Prz
+            // 
+            this.Prz.HeaderText = "Prz";
+            this.Prz.Name = "Prz";
+            this.Prz.ReadOnly = true;
+            this.Prz.Width = 80;
+            // 
+            // Reason1
+            // 
+            this.Reason1.HeaderText = "Reason 1";
+            this.Reason1.Name = "Reason1";
+            this.Reason1.ReadOnly = true;
+            this.Reason1.Width = 80;
+            // 
+            // Reason2
+            // 
+            this.Reason2.HeaderText = "Reason 2";
+            this.Reason2.Name = "Reason2";
+            this.Reason2.ReadOnly = true;
+            this.Reason2.Width = 80;
+            // 
+            // Reason3
+            // 
+            this.Reason3.HeaderText = "Reason 3";
+            this.Reason3.Name = "Reason3";
+            this.Reason3.ReadOnly = true;
+            this.Reason3.Width = 80;
+            // 
+            // Reason4
+            // 
+            this.Reason4.HeaderText = "Reason 4";
+            this.Reason4.Name = "Reason4";
+            this.Reason4.ReadOnly = true;
+            this.Reason4.Width = 80;
+            // 
+            // Reason5
+            // 
+            this.Reason5.HeaderText = "Reason 5";
+            this.Reason5.Name = "Reason5";
+            this.Reason5.ReadOnly = true;
+            this.Reason5.Width = 80;
+            // 
+            // Reason6
+            // 
+            this.Reason6.HeaderText = "Reason 6";
+            this.Reason6.Name = "Reason6";
+            this.Reason6.ReadOnly = true;
+            this.Reason6.Width = 80;
+            // 
+            // Reason7
+            // 
+            this.Reason7.HeaderText = "Reason 7";
+            this.Reason7.Name = "Reason7";
+            this.Reason7.ReadOnly = true;
+            this.Reason7.Width = 80;
+            // 
+            // Sum
+            // 
+            this.Sum.HeaderText = "Sum";
+            this.Sum.Name = "Sum";
+            this.Sum.ReadOnly = true;
+            this.Sum.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(495, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(782, 485);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btnLoad);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(782, 56);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.AutoSize = true;
+            this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 56);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(782, 429);
+            this.panel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Print";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 485);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(798, 96);
+            this.Name = "Form1";
+            this.Text = "S-Files";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prz;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sum;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Drawing.Printing;
+
+namespace S_Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Екземпляр приложения Excel
+        Excel.Application xlApp;
+        //Лист
+        Excel.Worksheet xlSheet;
+        //Выделеная область
+        Excel.Range xlSheetRange;
+        public string query = "";
+
+        public string[,] Enp;
+        public int Cou = 0;
+        public string przcod;
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            OpenFileDialog oFD = new OpenFileDialog();
+            oFD.Filter = "XML |*.xml";
+            oFD.Multiselect = true;
+            int r1 = 0;
+            int r2 = 0;
+            int r3 = 0;
+            int r4 = 0;
+            int r5 = 0;
+            int r6 = 0;
+            int r7 = 0;
+            int sum = 0;
+
+            DialogResult dr = oFD.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                Enp = new string[oFD.FileNames.Count(), 7];
+                Cou = oFD.FileNames.Count();
+                for (int i=0;i<oFD.FileNames.Count() ; i++) {
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(oFD.FileNames[i]);
+                    przcod = doc.DocumentElement.Attributes[3].InnerText;
+                    r1 = 0;
+                    r2 = 0;
+                    r3 = 0;
+                    r4 = 0;
+                    r5 = 0;
+                    r6 = 0;
+                    r7 = 0;
+                    sum = 0;
+                    foreach (XmlNode node in doc.DocumentElement)
+                    {
+                        if (node.Attributes[1].InnerText == "1")
+                        {
+                            r1++;
+                            Enp[i, 0] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        if (node.Attributes[1].InnerText == "2")
+                        {
+                            r2++;
+                            Enp[i, 1] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        if (node.Attributes[1].InnerText == "3")
+                        {
+                            r3++;
+                            Enp[i, 2] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        if (node.Attributes[1].InnerText == "4")
+                        {
+                            r4++;
+                            Enp[i, 3] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        if (node.Attributes[1].InnerText == "5")
+                        {
+                            r5++;
+                            Enp[i, 4] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        if (node.Attributes[1].InnerText == "6")
+                        {
+                            r6++;
+                            Enp[i, 5] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        if (node.Attributes[1].InnerText == "7")
+                        {
+                            r7++;
+                            Enp[i, 6] += node.Attributes["ENP"].InnerText + ";";
+                        }
+                        sum = r1 + r2 + r3 + r4 + r5 + r6 + r7;
+                    }
+                    dataGridView1.Rows.Add(przcod, r1.ToString(), r2.ToString(), r3.ToString(), r4.ToString(), r5.ToString(), r6.ToString(), r7.ToString(),sum.ToString());
+                }
+                r1 = 0;
+                r2 = 0;
+                r3 = 0;
+                r4 = 0;
+                r5 = 0;
+                r6 = 0;
+                r7 = 0;
+                sum = 0;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[1].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r1 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[2].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r2 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[3].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r3 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[4].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r4 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[5].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r5 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[6].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r6 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[7].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    r7 += incom;
+                }
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int incom;
+                    int.TryParse((row.Cells[8].Value ?? "0").ToString().Replace(".", ","), out incom);
+                    sum += incom;
+                }
+                dataGridView1.Rows.Add("ИТОГ", r1.ToString(), r2.ToString(), r3.ToString(), r4.ToString(), r5.ToString(), r6.ToString(), r7.ToString(), sum.ToString());
+
+            }
+            oFD.Dispose();
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private DataTable GetData()
+        {
+            //строка соединения
+            string connString = @"Network Library=DBMSSOCN;Data Source=192.168.1.101;Initial Catalog=Oms_Buryatiya;User ID=sa;Password=Kmsadmin_403;Persist Security Info=True;";
+
+            SqlConnection con = new SqlConnection(connString);
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand comm = new SqlCommand(query, con);
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(comm);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+            return dt;
+        }
+        void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void Create_Click(int col, int row)
+        {
+            xlApp = new Excel.Application();
+
+            try
+            {
+                //добавляем книгу
+                xlApp.Workbooks.Add(Type.Missing);
+                
+                //делаем временно неактивным документ
+                xlApp.Interactive = false;
+                xlApp.EnableEvents = false;
+
+                //выбираем лист на котором будем работать (Лист 1)
+                xlSheet = (Excel.Worksheet)xlApp.Sheets[1];
+                //Название листа
+                xlSheet.Name = "ПРЗ " + dataGridView1.Rows[row].Cells[0].Value.ToString() + " - " + dataGridView1.Columns[col].HeaderCell.Value.ToString() + " - " + dataGridView1.Rows[row].Cells[col].Value.ToString();//"Данные";
+                
+                //Выгрузка данных
+                DataTable dt = GetData();
+                
+
+                int collInd = 0;
+                int rowInd = 0;
+                string data = "";
+
+                //называем колонки
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    data = dt.Columns[i].ColumnName.ToString();
+                    xlSheet.Cells[1, i + 1] = data;
+
+                    //выделяем первую строку
+                    xlSheetRange = xlSheet.get_Range("A1:Z1", Type.Missing);
+
+                    //делаем полужирный текст и перенос слов
+                    xlSheetRange.WrapText = true;
+                    xlSheetRange.Font.Bold = true;
+                }
+
+                //заполняем строки
+                for (rowInd = 0; rowInd < dt.Rows.Count; rowInd++)
+                {
+                    for (collInd = 0; collInd < dt.Columns.Count; collInd++)
+                    {
+                        data = dt.Rows[rowInd].ItemArray[collInd].ToString();
+                        xlSheet.Cells.NumberFormat = "@";
+                        xlSheet.Cells[rowInd + 2, collInd + 1] = data;
+                    }
+                }
+                
+
+                //выбираем всю область данных
+                xlSheetRange = xlSheet.UsedRange;
+
+                //выравниваем строки и колонки по их содержимому
+                xlSheetRange.Columns.AutoFit();
+                xlSheetRange.Rows.AutoFit();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                //Показываем ексель
+                xlApp.Visible = true;
+
+                xlApp.Interactive = true;
+                xlApp.ScreenUpdating = true;
+                xlApp.UserControl = true;
+
+                //Отсоединяемся от Excel
+                releaseObject(xlSheetRange);
+                releaseObject(xlSheet);
+                releaseObject(xlApp);
+            }
+        }
+        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex > 0 && e.ColumnIndex < 8 && e.RowIndex >= 0 && e.RowIndex < Cou)
+                if (Enp[e.RowIndex, e.ColumnIndex - 1] != "" && Enp[e.RowIndex, e.ColumnIndex - 1] != null) 
+                {
+                    try
+                    {
+                        string s = "";
+                        int i = 1;
+                        String[] words = Enp[e.RowIndex, e.ColumnIndex - 1].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (string word in words)
+                        {
+                            if (i == words.Count())
+                            {
+                                s += "ENP='" + word + "'";
+                            }
+                            else
+                            {
+                                i++;
+                                s += "ENP='" + word + "' or ";
+                            }
+                        }
+                        query = @"SELECT a.[IDPRZ],[ENP],[Surname],[Name1],[Name2],[Sex],CONVERT(DATE,[Birthday]) AS Birthday,[Phone],[Addr]	  
+                                    FROM [Oms_Buryatiya].[dbo].[Pers] a left join [Oms_Buryatiya].[dbo].[Address] on (IDAddressOwner=IDPers and IDAddressType='35021') 
+                                    WHERE " + s + " ORDER BY [Surname],[Name1],[Name2],[Sex],[Birthday]";
+                        toolTip1.Show("Запрос к БД", this.label1, 500);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                    finally
+                    {
+                        Create_Click(e.ColumnIndex, e.RowIndex);
+                    }
+                }
+            
+        }
+
+        Bitmap bmp;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            PrintPreviewDialog dPP = new PrintPreviewDialog();
+            int height = dataGridView1.Height;
+            int height1 = (dataGridView1.RowCount + 1) * dataGridView1.RowTemplate.Height+2;
+            dataGridView1.Dock = DockStyle.None;
+            dataGridView1.Height = height1;
+            //MessageBox.Show(height1 + " " + height);
+            bmp = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bmp, new Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+            dataGridView1.Height = height;
+            dataGridView1.Dock = DockStyle.Fill;
+            PrintDocument dP = new PrintDocument();
+            dP.PrintPage += dP_PrintPage;
+            dPP.Document = dP;
+            //bmp.Save("dpp.jpg",System.Drawing.Imaging.ImageFormat.Jpeg);
+            dPP.ShowDialog();
+
+        }
+
+        private void dP_PrintPage (object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, 0, 0);
+        }
+    }
+}
